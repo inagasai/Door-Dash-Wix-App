682d331c0fb1db578c56af4c106da37a7540b48d
nagkhumbu <nagasai.mudara@khumbusystems.com>
2021-07-30 16:19:20 +0530

    Setting Page with Live Refresh

--- settings.js
+++ settings.js
@@ -15,63 +15,66 @@
         }       
       });  
     }
+
     advanced_settings();
+    attachListeners();
 
-     
-     attachListeners();
-   // $(attachListeners);
-
-
-   Wix.Data.Public.set("startCounter",25, { scope: 'APP' },
-        function(d) { console.log(d) }, function(f) { console.log(f) }
-   );
-    
+    /*Wix.Data.Public.set("startCounter",25, { scope: 'APP' },
+      // function(d) { console.log(d) }, function(f) { console.log(f) }
+    );*/    
   })
 })(jQuery);
 
 
-function onUpdate(key, value) { 
-  //Wix.Settings.triggerSettingsUpdatedEvent({key: key, value: value});
-  //Wix.Settings.triggerSettingsUpdatedEvent({key: key+'Fast', value: "123456"});
-  console.log(key);
-  console.log(value);
-  console.log(Wix);
-  Wix.Data.Public.set(key, value, { scope: 'APP' },
-       function(d) { console.log(d) }, function(f) { console.log(f) }
-  );
-  //Wix.Settings.triggerSettingsUpdatedEvent({key: 'dropdown2', value: 0});
-  Wix.Styles.getStyleParams(style => {
-    console.log(style);
-  });  
-  //Wix.UI.set('messagePlaceholder',"fast");  
-
-}
-function getPublic($element, ctrl) { 
-  if($element.attr('wix-ctrl')!="Input") return;
-  console.log('Input');
-  var key=$element.attr('wix-model');
-  if(!key) return;
-  console.log('key');
-  console.log(key);   
-  Wix.Data.Public.get(key, { scope: 'APP' }, 
-    function(d){console.log(d);  ctrl.setValue(d[key]);}, 
-    function(f){console.log(f)}
-  );
-  console.log(ctrl); 
- 
-}
-
 function attachListeners() {
+  console.log("----------------------------------------------------------"); 
+  console.log("---------------------GETTING DATA :SETTINGS---------------"); 
   $('[wix-ctrl]').each(function (index, element) {
     var $element = $(element);
     var ctrl = $element.getCtrl();
+
+    getPublic($element, ctrl);  
+    
     if ($.isFunction(ctrl.onChange)) {
       ctrl.onChange(function (value) {
+        console.log("----------------------------------------------------------"); 
+        console.log("---------------------UPDATE DATA :SETTINGS---------------"); 
         onUpdate($element.attr('wix-param'), value);
+        //console.log("Settings ctrl onChange");  console.log(value);  
       })
-    } 
-    getPublic($element, ctrl);  
-    console.log("ctrl");   
-    //console.log(ctrl);   
+    }     
+    //console.log("Settings public ctrl"); console.log(index);   
   });
+}
+
+function onUpdate(key, value) { 
+  //Wix.Settings.triggerSettingsUpdatedEvent({key: key, value: value});
+  //Wix.Settings.triggerSettingsUpdatedEvent({key: key+'Fast', value: "123456"});
+  //console.log(key);
+  //console.log(value);
+  //console.log(Wix);
+  console.log("Settings : "+key+" : "+ value);
+  Wix.Data.Public.set(key, value, { scope: 'APP' },
+       function(d) { console.log(d) }, 
+       function(f) { console.log(f) }
+  );
+  //Wix.Settings.triggerSettingsUpdatedEvent({key: 'dropdown2', value: 0});
+  Wix.Styles.getStyleParams(style => {
+    console.log("All styles");
+    console.log(style);
+  });  
+  //Wix.UI.set('messagePlaceholder',"fast");  
+}
+
+function getPublic($element, ctrl) { 
+  if($element.attr('wix-ctrl')!="Input") return;
+  //console.log('Settings Input');
+  var key=$element.attr('wix-model');
+  if(!key) return;  
+  console.log('Settings key : '+ key );
+  Wix.Data.Public.get(key, { scope: 'APP' }, 
+    function(d){console.log("Settings Public success : "+key);  console.log(d);  ctrl.setValue(d[key]);}, 
+    function(f){console.log("Settings Public fail : "+key); console.log(f)}
+  );
+  //console.log(" Settings ctrl"); console.log(ctrl);  
 }
--- widget.js
+++ widget.js
@@ -1,5 +1,6 @@
 var SDK_loaded=false;
-var data={}; count=0; style_data=false; editor=false;     
+var data={}, count=0, style_data=false, editor=false;  
+var SDK=false;
 
 console.log("Widget");
 
@@ -7,10 +8,11 @@ console.log("Widget");
 (function($){
     $(document).ready(function(){
         var $body=$("body");
-        console.log('Ready');            
-        console.log('Next');        
+        console.log('Widget Ready');            
+        console.log('Widget Next');        
 
-        loadData();
+        loadPublicData();
+        loadStyleData();
 
         Wix.addEventListener(Wix.Events.SETTINGS_UPDATED, onSettingsUpdate);
         // You can get the style params programmatically, un-comment the following snippet to see how it works:
@@ -20,95 +22,120 @@ console.log("Widget");
             //$('.sample-settings-title').show();
            // $('.json').html(update);
            // updateCompHeight();
-            console.log("Setting Update");
+            console.log("Widget Setting Update");
             console.log(update);
             //data={}; count=0; style_data=false; editor=true;         
             //loadData();
         }
+        Wix.addEventListener(Wix.Events.PUBLIC_DATA_CHANGED, onPublicUpdate); 
 
+        function onPublicUpdate(update) {        
+            console.log("----------------------------------------------------------"); 
+            console.log("---------------------PUBLIC DATA : WIDGET---------------"); 
+            data={}; count=0; style_data=false; editor=true;  
+            console.log("SDK"); console.log(SDK);
+            loadPublicData();        
+            loadStyleData();
+            $.each(update, function(i, v){
+                if(i=="_buttonPosition") {_buttonPosition(v);}                
+                else if(i=="_buttonAlignment") {_buttonAlignment(v);}
+                else if(i=="_floatingBar") {style_data.booleans._floatingBar=v;}
+                else if(i=="_floatingBar") {style_data.booleans._floatingBar=v;}
+                console.log("update: "+i+' : '+v); 
+                console.log(style_data);    
+            })           
+            console.log(update);            
+            console.log(data);     
+        }
       
         // You can also get the style every time it changes, try this:
-        Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
-            console.log("Style Update");
+       /* Wix.addEventListener(Wix.Events.STYLE_PARAMS_CHANGE, style => {
+            console.log("----------------------------------------------------------"); 
+            console.log("---------------------STYLES DATA : WIDGET---------------");            
             console.log(style);
-            data={}; count=0; style_data=false; editor=true; 
-            //$body.html(" ");        
+            data={}; count=0; style_data=false; editor=true;             
             //loadData();
-        });
-
-
-
-        function loadData(){
+        });*/
+        function loadPublicData(){
             // Wix.Data.Public.get("startCounter", { scope: 'APP' }, function(d){console.log(d); data.counter=d.startCounter; run();}, function(f){console.log(f)});
             Wix.Data.Public.get("_businessID", { scope: 'APP' }, function(d){console.log(d); data._businessID=d._businessID; count++; run();}, function(f){console.log(f)});
-            Wix.Data.Public.get("_buttonText", { scope: 'APP' }, function(d){console.log(d); data._buttonText=d._buttonText; count++; run();}, function(f){console.log(f)});
+            Wix.Data.Public.get("_buttonText", { scope: 'APP' }, function(d){console.log(d); data._buttonText=d._buttonText; count++; run();}, function(f){console.log(f)});            
+        }
+        function loadStyleData(){         
             Wix.Styles.getStyleParams(style => {
                 style_data=style;
-                console.log(style);
-                if(style_data.numbers._buttonAlignment==1){
-                    style_data._buttonAlignment='left';
-                }else if(style_data.numbers._buttonAlignment==2){
-                    style_data._buttonAlignment='center';
-                }else if(style_data.numbers._buttonAlignment==3){
-                    style_data._buttonAlignment='right';
-                }else{
-                    style_data._buttonAlignment='';
-                }
-
-                if(style_data.numbers._buttonPosition==1){
-                    style_data._buttonPosition='top';
-                }else if(style_data.numbers._buttonPosition==2){
-                    style_data._buttonPosition='bottom';
-                }else{
-                    style_data._buttonPosition='';
-                }
+                console.log(style);                
+                _buttonAlignment(style_data.numbers._buttonAlignment);
+                _buttonPosition(style_data.numbers._buttonPosition);
                 run();            
             });
         }
-
+        function _buttonAlignment(v){             
+            var v_='';         
+            if(v==1){
+                v_='left';
+            }else if(v==2){
+                v_='center';
+            }else if(v==3){
+                v_='right';
+            }
+            style_data._buttonAlignment=v_;
+        } 
+        function _buttonPosition(v){             
+            var v_='';  
+            if(v==1){
+               v_='top';
+            }else if(v==2){
+                v_='bottom';
+            }
+            style_data._buttonPosition=v_;
+        } 
         function run(){  
             console.log(data);          
             if(count==2 && style_data)  {
-                console.log("DATA");
+                //console.log("Widget STYLE DATA");
                 console.log(style_data);
+                //console.log("Widget DATA");
                 console.log(data);
                 create();
             }
-        }  
-
-        /*
-        console.log( "Wix.Styles");
-        console.log( Wix.Styles);
-        $('.navtohome').click(() => {
-            Wix.getSiteMap(pages => {
-            Wix.navigateToPage(pages[0].pageId.substring(1));
-            });
-            console.log('navigated');
-        });
-        */
+        }       
         
-        
-        function create(){           
-            //if(typeof StorefrontSDK == "undefined"){
+        function create(){ 
+           
+            if(!window['StorefrontSDK']){
                 !(function (e, t, r, n) {
-                    var o, c, s;
-                    (o = e.document),
-                        (c = t.children[0]),
-                        (s = o.createElement("script")),
-                        (e.StorefrontSDKObject = "StorefrontSDK"),
-                        (e[e.StorefrontSDKObject] = {
-                            executeCommand: function (t, r) {
-                                e[e.StorefrontSDKObject].buffer.push([t, r]);
-                            },
-                            buffer: [],
-                        }),
-                        (s.async = 1),
-                        (s.src = "https://web-apps.cdn4dd.com/webapps/sdk-storefront/latest/sdk.js"),
-                        t.insertBefore(s, c);
+                    console.log("SDK SCRIPT 2| WIDGET");   
+                
+                    var o, c, s;               
+
+                    o = e.document;
+                    s = o.createElement("script");
+                    
+                    s.async = 1;
+                    s.src = "https://web-apps.cdn4dd.com/webapps/sdk-storefront/latest/sdk.js";
+
+                    c = t.children[0];
+                    t.insertBefore(s, c);
+
+                    e.StorefrontSDKObject = "StorefrontSDK";
+                    e[e.StorefrontSDKObject] = {
+                        executeCommand: function (t, r) {
+                            console.log("SDK EXECUTE | WIDGET");
+                            e[e.StorefrontSDKObject].buffer.push([t, r]);
+                        },
+                        buffer: [],
+                    };
+
+                
+                    
+
                 })(window, document.head); 
-                SDK_loaded=true;
-            //}
-            StorefrontSDK.executeCommand("renderFloatingButton", {
+            }
+            
+            SDK_loaded=true;
+
+            var prop={
                 businessId: data._businessID,
                 buttonText: data._buttonText,
                 position: style_data._buttonPosition,
@@ -118,7 +145,33 @@ console.log("Widget");
                 floatingBar: style_data.booleans._floatingBar,
                 backgroundColor: style_data.colors._floatingBarColor.value,
                 urlParams: { utm_medium: "wix_app" },
-            });
+            };
+
+            if(window['StorefrontSDK']){
+                $body.html(" FASTY ");
+                console.log("SDK EXISTS");
+                console.log(window['StorefrontSDK']);
+                var SDK_=window['StorefrontSDK'];
+                if(SDK_['executeCommand']){
+                    console.log("SDK FIRST");
+                    SDK_['executeCommand']("renderFloatingButton", prop);
+                } else if(SDK_['executeCmd']){
+                    console.log("SDK NEXT");
+                    SDK_['executeCmd']("renderFloatingButton", prop);
+                }           
+            }  
+
+            /*StorefrontSDK.executeCommand("renderFloatingButton", {
+                businessId: data._businessID,
+                buttonText: data._buttonText,
+                position: style_data._buttonPosition,
+                buttonBackgroundColor: style_data.colors._buttonBackgroundColor.value,
+                buttonTextColor: style_data.colors._buttonTextColor.value,
+                buttonAlignment: style_data._buttonAlignment,
+                floatingBar: style_data.booleans._floatingBar,
+                backgroundColor: style_data.colors._floatingBarColor.value,
+                urlParams: { utm_medium: "wix_app" },
+            });*/
 
             /*StorefrontSDK.executeCommand("renderFloatingButton", {
                 businessId: 1234,
@@ -134,5 +187,5 @@ console.log("Widget");
         }
        
     });
-    console.log('jQuery');
+    console.log('Widget jQuery');
 })(jQuery);
